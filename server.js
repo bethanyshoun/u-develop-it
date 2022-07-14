b93e653393958ae7a34453d8729d0e9d2c0b15cf
bethanyshoun <bethanyshoun@gmail.com>
2022-07-14 19:34:50 -0400

    initial redone for security

---
+++ server.js
@@ -0,0 +1,92 @@
+//connect to mysql database
+const mysql = require('mysql2');
+
+const express = require('express');
+
+//PORT designation
+const PORT = process.env.PORT || 3001;
+// app expression
+const app = express();
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'mySQL123',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+  /*
+  //Delete a candidate
+  db.query('DELETE FROM candidates WHERE id = ?', 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+  });
+*/
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+  VALUES (?,?,?,?)`;
+
+  const params = [1, "Ronald", "Firbank", 1];
+/*
+  db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+  });
+*/
+//route to handle user requests that aren't supported by the app
+//Default response for any other request (Not Found) **Must be placed after all other routes**
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+//function to start the Express.js server on port 3001
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
+});
+
